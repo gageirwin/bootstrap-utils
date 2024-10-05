cb203b8e6ae560386594624e9a1c573a87fac141
Gage Irwin <91108814+gageirwin@users.noreply.github.com>
2024-10-04 20:49:35 -0500

    Update datetime.js
    display input field values

--- static/js/datetime.js
+++ static/js/datetime.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
-    $("#test_time-time, #test_time-text").DateTimeInput({ type: "time" })
-    $("#test_date-text, #test_date-date").DateTimeInput({ type: "date" })
-    const test1 = $("#datetime-text").DateTimeInput({ type: "datetime" })
-    const test2 = $("#datetime-datetime").DateTimeInput({ type: "datetime" })
-    console.log(test1)
-    console.log(test2)
+    $("#input-time").DateTimeInput()
+    $("#input-date").DateTimeInput()
+    $("#input-datetime").DateTimeInput()
+
+
+    $(`input`).on("change", function (e) {
+        const $this = $(this)
+        $this.siblings().closest("span").text($this.val())
+    })
+
 })
